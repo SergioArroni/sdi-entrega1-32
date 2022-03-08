ef34d5c5ffb48673c9b21344803d9dfed5fd52ee
Sergio Arroni <uo276341@uniovi.es>
2022-03-08 18:48:15 +0100

    SDI-32-Inicializacion del proyecto

---
+++ src/main/java/com/uniovi/sdientrega132/SdiEntrega132Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdientrega132;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega132Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdiEntrega132Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdientrega132/SdiEntrega132ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdientrega132;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega132ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
