28a37828eef652333963c0aa984ccba0e5166635
uo264197 <uo264197@uniovi.es>
2022-03-11 10:56:49 +0100

    Añadidos los metodos get, set y constructor de la clase Publication

--- src/main/java/com/uniovi/sdientrega132/entities/Publication.java
+++ src/main/java/com/uniovi/sdientrega132/entities/Publication.java
@@ -17,6 +17,50 @@ public class Publication {
     @JoinColumn(name="user_id")
     private User user;
 
+    public Publication(String title, String text, User user) {
+        this.title=title;
+        this.text=text;
+        this.user=user;
+        publishingDate = new Date();
+    }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(Date publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
