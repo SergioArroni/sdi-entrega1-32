a06671ca79f74c4b56eea3ed5d6d46989f7ea180
Nuria <UO277418@uniovi.es>
2022-03-19 09:15:49 +0100

    SDI-2122-311-Vista de logs

--- src/main/java/com/uniovi/sdientrega132/controllers/UsersController.java
+++ src/main/java/com/uniovi/sdientrega132/controllers/UsersController.java
@@ -46,8 +46,7 @@ public class UsersController {
         user.setRole(rolesService.getRoles()[0]);
         usersService.addUser(user);
         securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
-        return "redirect:/user/list";
-        //return "redirect:/home";
+        return "redirect:/home";
     }
 
     @RequestMapping("/user/list")
